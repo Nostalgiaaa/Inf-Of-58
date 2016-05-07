425fadab657e330b15b7c6f2ae77d9d0d561bd27
Nostalgiaaa <844255371@qq.com>
2016-05-07 20:41:38 +0800

    Update and rename ipad.txt to ipad.py

--- ipad.py
+++ ipad.py
@@ -1,21 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
-main_url = 'http://bj.58.com/pbdn/0/pn1'
+
+
+def who_sell(who_sells=0):
+    list_view = 'http://bj.58.com/pbdn/{}/pn1/'.format(str(who_sells))
+    return list_view
+main_url = who_sell(0)
 web_data = requests.get(main_url)
 soup = BeautifulSoup(web_data.text,'lxml')
-'''
-#content > div.person_add_top.no_ident_top > div.per_ad_left > div.col_sub.mainTitle > h1
-#index_show > ul.mtit_con_left.fl > li.timeaa
-
-'''
 select = soup.select('td.t a.t')
-leimu = main_url[:22]
+
+leimu = who_sell(0)[:22]
 print(leimu)
 page_link = []
 for href in select:
     page_link.append(href.get('href'))
 
+
+def get_count(url):
+    id = url.split('/')[-1].strip('x.shtml')
+    api = 'http://jst1.58.com/counter?infoid={}'.format(id)
+    headers = {'Referer': 'http://bj.58.com/pingbandiannao/{}'.format(id),
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'}
+    js = requests.get(api, headers=headers)
+
+    views = js.text.split('=')[-1]
+    return views
 
 for url in page_link:
     inf = requests.get(url)
@@ -29,7 +40,7 @@ for url in page_link:
     else:
         title=titles[0].get_text()
     if times == []:
-        time = ['null']
+        time = ['空']
     else:
         time = times[0].text
     if prices == []:
@@ -39,16 +50,18 @@ for url in page_link:
     if places == []:
         place = ['null']
     else:
-        place = places[0].text[1:3]
-
+        place = places[0].text[1:3]+places[0].text[-3:-1]
     data = {
         'title': title,
         'time': time,
         'price': price,
         'places': place,
+        'count': get_count(url),
+        '类型': '个人' if(main_url[22:23] == '0') else '商家'
     }
 
-    if(data!={'title': ['null'], 'price': ['null'], 'places': ['null'], 'time': ['null']}):
+    if(title!=['null']):
         print(data)
+
 
 
